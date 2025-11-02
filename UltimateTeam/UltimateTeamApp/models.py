a92a3bd8f79d984f5bc81f12a38af2a93a8a8f2f
Diego <diegogarciaangel25@gmail.com>
2025-11-02 17:09:53 +0100

    creación del modelo Usuario

--- UltimateTeam/UltimateTeamApp/models.py
+++ UltimateTeam/UltimateTeamApp/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+from django.db import models
+
+class Usuario(models.Model):
+
+    nombre = models.CharField(max_length=100, unique=True)
+    email = models.EmailField(max_length=100, unique=True)
+    fecha_creacion = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.nombre
--- UltimateTeam/UltimateTeamApp/views.py
+++ UltimateTeam/UltimateTeamApp/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.http import JsonResponse
 
 # Create your views here.
+
+from rest_framework import viewsets, status
+from .models import Usuario
+
+class UsuarioViewSet(viewsets.ModelViewSet):
+
+    def get_usuario(request, id):
+        if request.method == "GET":
+            try:
+                usuario = Usuario.objects.values().get(id=id)
+                return JsonResponse(usuario)
+            except Usuario.DoesNotExist:
+                return JsonResponse({"error": "Usuario no encontrado"}, status=404)
+        else:
+            return JsonResponse({"error": "Operación no soportada"})
